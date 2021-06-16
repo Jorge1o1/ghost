45091a764ac3641311e293f935cf96b10ae6d7de
Jorge Martinez <jlmartinez@acropolismarketing.com>
2021-06-16 15:24:45 -0400

    Use brown corpus

--- scripts/filtercorpus.py
+++ scripts/filtercorpus.py
@@ -1,8 +1,12 @@
-ALLOWED_LETTERS = ['D','O','G']
+ALLOWED_LETTERS = ['D','O','G','E']
+MIN_LETTERS = 4
 
 def uses_only_allowed(word):
     return all(l in ALLOWED_LETTERS for l in word.rstrip())
 
-with open("corpus.sample", "r") as infile:
-    with open("corpus.txt", "w") as outfile:
-        outfile.writelines(filter(uses_only_allowed,infile))
+def is_longer_than_minimum(word):
+    return len(word) > MIN_LETTERS
+
+with open("../data/corpus.sample", "r") as infile:
+    with open("../data/corpus.txt", "w") as outfile:
+        outfile.writelines(filter(is_longer_than_minimum, filter(uses_only_allowed,infile)))
---
+++ scripts/makecorpusfrombrown.py
@@ -0,0 +1,17 @@
+MIN_LETTERS = 3
+TOP_N = 1000
+
+def get_count_from_line(line):
+    parts = line.split()
+    return int(parts[1]) if len(parts) > 1 else 0
+
+def is_longer_than_minimum(line):
+    word = line.split()[0]
+    return len(word) > MIN_LETTERS
+
+with open("../data/brown_freq.txt", "r") as infile:
+    sorted_qty = sorted(infile, key=get_count_from_line, reverse=True)
+    valid = filter(is_longer_than_minimum, sorted_qty[:TOP_N])
+    sorted_alpha = [l.split()[0] + "\n" for l in sorted(valid)]
+    with open("../data/corpus2.txt", "w") as outfile:
+        outfile.writelines(sorted_alpha)
