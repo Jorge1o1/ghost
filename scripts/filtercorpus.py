aa0d5e212626aee68c4af19dec33c4b56d41cb0d
Jorge Martinez <jlmartinez@acropolismarketing.com>
2021-06-16 15:26:15 -0400

    More corpus changes

--- scripts/filtercorpus.py
+++ scripts/filtercorpus.py
@@ -1,4 +1,4 @@
-ALLOWED_LETTERS = ['D','O','G','E']
+ALLOWED_LETTERS = ['A','B','C','D','E']
 MIN_LETTERS = 4
 
 def uses_only_allowed(word):
@@ -7,6 +7,6 @@ def uses_only_allowed(word):
 def is_longer_than_minimum(word):
     return len(word) > MIN_LETTERS
 
-with open("../data/corpus.sample", "r") as infile:
-    with open("../data/corpus.txt", "w") as outfile:
+with open("../data/corpus.txt", "r") as infile:
+    with open("../data/corpus.sample", "w") as outfile:
         outfile.writelines(filter(is_longer_than_minimum, filter(uses_only_allowed,infile)))
